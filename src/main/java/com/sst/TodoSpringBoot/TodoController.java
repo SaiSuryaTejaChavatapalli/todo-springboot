176b42c31b52bdb5a9ed587809eeb185cff97ae4
Sai Surya Teja <saisuryateja3@gmail.com>
2024-12-13 13:42:23 +0530

    Handled status code in response using ResponseEntity

--- src/main/java/com/sst/TodoSpringBoot/TodoController.java
+++ src/main/java/com/sst/TodoSpringBoot/TodoController.java
@@ -1,10 +1,9 @@
 package com.sst.TodoSpringBoot;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,14 +20,15 @@ public class TodoController {
     }
 
     @GetMapping("/todos")
-    public List<Todo> getTodos(){
-        return todos;
+    public ResponseEntity<List<Todo>>  getTodos(){
+        return ResponseEntity.ok(todos);
     }
 
     @PostMapping("/todos")
-    public Todo createTodo(@RequestBody Todo newTodo){
+//    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<Todo> createTodo(@RequestBody Todo newTodo){
         todos.add(newTodo);
-        return  newTodo;
+        return  ResponseEntity.status(HttpStatus.CREATED).body(newTodo) ;
     }
 
 }
